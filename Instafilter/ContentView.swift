e39ecd7de24785c7358092fb15991f1f6ad650e5
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-11-18 21:28:22 +0530

    first commit

---
+++ Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by PRABALJIT WALIA     on 18/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
