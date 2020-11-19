ed6f5b86dbdfe783a1c71e1184bec18bbcdcc877
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-11-19 19:49:11 +0530

    core image filters

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -4,13 +4,42 @@
 //
 //  Created by PRABALJIT WALIA     on 18/11/20.
 //
-
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image:Image?
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform:loadImage)
+    }
+    func loadImage(){
+        image = Image("")
+        guard let inputImage = UIImage(named: "")
+        else {return}
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.pixellate()
+        
+        currentFilter.inputImage = beginImage
+        currentFilter.scale = 100
+        
+        // get a CIImage from our filter or exit if that fails
+        guard let outputImage = currentFilter.outputImage
+        else{return}
+        
+        // attempt to get a CGImage from our CIImage
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent){
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
