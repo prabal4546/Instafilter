a23041625bed2481068bb747eb5804526d6d40fd
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-11-27 12:07:08 +0530

    basic ui

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -9,27 +9,52 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var image:Image?
-    @State private var showingImagePicker = false
-    @State private var inputImage:UIImage?
+    @State private var filterIntensity = 0.5
     var body: some View {
         
-        VStack{
-            image?
-                .resizable()
-                .scaledToFit()
-            Button("select image"){
-                self.showingImagePicker = true
+        NavigationView{
+            VStack{
+                ZStack{
+                    Rectangle()
+                        .fill(Color.secondary)
+                    if image != nil{
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    else{
+                        Text("Tap to select a picture")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                    
+                }
+                .onTapGesture {
+                    //select an image
+                }
+                HStack{
+                    Text("Intensity")
+                        .padding(.horizontal)
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                HStack{
+                    Button("Change Filter"){
+                        //change filter
+                    }
+                    Spacer()
+                    Button("Save"){
+                        //save the picture
+                    }
+                }
             }
+            
         }
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage){
-            ImagePicker(image: self.$inputImage)
-        }
-    }
-    func loadImage(){
-        guard let inputImage = inputImage else {return}
-        image = Image(uiImage: inputImage)
+        .padding([.horizontal,.bottom])
+        .navigationBarTitle("Instafilter")
     }
 }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
