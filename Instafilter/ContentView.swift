8c196531becb738337c91a3a507e1e15af378c32
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-11-30 12:37:13 +0530

    added sepia filter

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -4,15 +4,32 @@
 //
 //  Created by PRABALJIT WALIA     on 18/11/20.
 //
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
     
     @State private var image:Image?
     @State private var filterIntensity = 0.5
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    
+    @State var currentFilter:CIFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     var body: some View {
         
-        NavigationView{
+        let intensity = Binding<Double>{
+            get:  do {
+                self.filterIntensity
+            },
+            set:do {
+                self.filterIntensity = $0
+                self.applyProcessing()
+            }
+        }
+        
+        return NavigationView{
             VStack{
                 ZStack{
                     Rectangle()
@@ -31,11 +48,12 @@ struct ContentView: View {
                 }
                 .onTapGesture {
                     //select an image
+                    self.showingImagePicker = true
                 }
                 HStack{
                     Text("Intensity")
                         .padding(.horizontal)
-                    Slider(value: $filterIntensity)
+                    Slider(value: intensity)
                 }
                 .padding(.vertical)
                 HStack{
@@ -52,6 +70,27 @@ struct ContentView: View {
         }
         .padding([.horizontal,.bottom])
         .navigationBarTitle("Instafilter")
+        .sheet(isPresented: $showingImagePicker,onDismiss: loadImage){
+            ImagePicker(image: self.$inputImage)
+        }
+    }
+    func applyProcessing(){
+        currentFilter.intensity = Float(filterIntensity)
+        guard let outputImage = currentFilter.outputImage
+        else{return}
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent){
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+            
+        }
+        
+    }
+    func loadImage(){
+        guard let inputImage = inputImage else {return}
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+        
     }
 }
     
--- Instafilter/ImagePicker.swift
+++ Instafilter/ImagePicker.swift
@@ -6,18 +6,13 @@
 //
 
 import SwiftUI
-
-struct ImagePicker: UIViewControllerRepresentable{
-    @Binding var image: UIImage?
-    @Environment(\.presentationMode) var presentationMode
-    class Coordinator:NSObject,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
-        
-        var parent:ImagePicker
-
+struct ImagePicker:UIViewControllerRepresentable {
+    
+    class Coordinator:NSObject,UINavigationControllerDelegate,UIImagePickerControllerDelegate{
+        let parent:ImagePicker
         init(_ parent: ImagePicker) {
             self.parent = parent
         }
-        
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             if let uiImage = info[.originalImage] as? UIImage{
                 parent.image = uiImage
@@ -25,18 +20,21 @@ struct ImagePicker: UIViewControllerRepresentable{
             parent.presentationMode.wrappedValue.dismiss()
         }
     }
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-    func makeUIViewController(context: Context) -> UIImagePickerController {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var image:UIImage?
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
         let picker = UIImagePickerController()
-        picker.delegate = context.coordinator
         return picker
     }
-    
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
         
     }
-    
-    
+
 }
+        
